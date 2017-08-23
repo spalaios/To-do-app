4b762e02bebb34d53c5804e5c75387598769f26f
Suraj Kumar Palai <palaisuraj@gmail.com>
2017-08-24 03:03:54 +0530

    Comment editting.

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var data= (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('
   completed:[]
 };
 
-// console.log(localStorage.getItem('todoList'));
+
 //SVG paths
 var removeSVG='<svg class="fill" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 26 26" version="1.1" width="26" height="26"> <g id="surface1"> <path class="fill" style=" " d="M 11 -0.03125 C 10.164063 -0.03125 9.34375 0.132813 8.75 0.71875 C 8.15625 1.304688 7.96875 2.136719 7.96875 3 L 4 3 C 3.449219 3 3 3.449219 3 4 L 2 4 L 2 6 L 24 6 L 24 4 L 23 4 C 23 3.449219 22.550781 3 22 3 L 18.03125 3 C 18.03125 2.136719 17.84375 1.304688 17.25 0.71875 C 16.65625 0.132813 15.835938 -0.03125 15 -0.03125 Z M 11 2.03125 L 15 2.03125 C 15.546875 2.03125 15.71875 2.160156 15.78125 2.21875 C 15.84375 2.277344 15.96875 2.441406 15.96875 3 L 10.03125 3 C 10.03125 2.441406 10.15625 2.277344 10.21875 2.21875 C 10.28125 2.160156 10.453125 2.03125 11 2.03125 Z M 4 7 L 4 23 C 4 24.652344 5.347656 26 7 26 L 19 26 C 20.652344 26 22 24.652344 22 23 L 22 7 Z M 8 10 L 10 10 L 10 22 L 8 22 Z M 12 10 L 14 10 L 14 22 L 12 22 Z M 16 10 L 18 10 L 18 22 L 16 22 Z "/> </g> </svg>';
 
@@ -17,7 +17,6 @@ addbtn.addEventListener('click',function(){
   var value=document.getElementById('item').value;
   if(value) {
     data.todo.push(value);
-    // console.log(data);
     addItemTodo(value);
     document.getElementById('item').value='';
   }
@@ -29,16 +28,14 @@ document.getElementById('item').addEventListener('keydown',function(e){
   var value=this.value;
   if(e.code === 'Enter' && value){
     data.todo.push(value);
-    // console.log(data);
     addItemTodo(value);
     document.getElementById('item').value='';
   }
 })
 
 function renderStart(){
-  // var localObject=JSON.parse(localStorage.getItem('todoList'));
-  // console.log(localObject);
-if(!data.todo.lenght && !data.completed.length) return;
+  
+  if(!data.todo.lenght && !data.completed.length) return;
 
   for(var i=0;i<data.todo.length;i++){
     addItemTodo(data.todo[i]);
@@ -50,13 +47,11 @@ if(!data.todo.lenght && !data.completed.length) return;
 }
 
 function objectUpdated(){
-  // console.log(JSON.stringify(data));
-  localStorage.setItem('todoList',JSON.stringify(data));
+ localStorage.setItem('todoList',JSON.stringify(data));
 }
 
 //logic for delete function
 function  removeItem(){
-  // console.log('this'+this);
   var item=this.parentNode.parentNode;
   var parent=item.parentNode;
   var id=parent.id;
@@ -67,8 +62,7 @@ function  removeItem(){
   }else{
     data.completed.splice(data.completed.indexOf(value),1);
   }
-  // console.log(data);
-  objectUpdated();
+   objectUpdated();
 }
 
 //logic for complete function
@@ -87,8 +81,7 @@ function completeItem(){
 
   }
 
-  // console.log(data);
-  // console.log(item.innerText);
+ 
   var target = (id ==='todo') ? document.getElementById('completed'):document.getElementById('todo');
   parent.removeChild(item);
   target.insertBefore(item,target.childNodes[0]);
